11b234bf46e67075ccfba3889ebade1d8f689cb3
Donnie <82501498+DonLeb95@users.noreply.github.com>
2024-11-26 00:20:35 -0400

    Create function for the computer's choice

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let compChoice = Math.floor( (Math.random() * 100) );
+    //(0 32) (33 65) (66 99)
+    if (compChoice <= 32) {
+        return "rock"
+    } else if (compChoice > 32 && compChoice <= 65) {
+        return "paper"
+    } else return "scissor"
+}
