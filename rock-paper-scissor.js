8f48dd041e6f4501765975d8a95ea2a8af575b14
Donnie <82501498+DonLeb95@users.noreply.github.com>
2024-11-26 01:07:23 -0400

    Add a function for the users prompt  choice

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,3 +1,4 @@
+// Makes the computer choice
 function getComputerChoice(){
     let compChoice = Math.floor( (Math.random() * 100) );
     //(0 32) (33 65) (66 99)
@@ -7,3 +8,16 @@ function getComputerChoice(){
         return "paper"
     } else return "scissor"
 }
+
+//Prompts the user for their choice. 
+function getHumanChoice(){
+    let myChoice = prompt('Choose between rock, paper or scissor and type it.Your opponent will be your computer :)', 'Type "rock", "paper" or "scissor"');
+    
+    if (myChoice != "rock" && myChoice != "paper" && myChoice != "scissor"){
+        alert('Looks like there was a mistake somewhere.Please try again');
+        return;
+    }
+
+    return myChoice;
+}
+
