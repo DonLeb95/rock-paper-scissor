273d574ea30888599d35b93d60537c429109c4f2
Donnie <82501498+DonLeb95@users.noreply.github.com>
2024-11-27 00:57:55 -0400

    Add round system for the game
    Got it to output the log and the addition of points to the winner as anticipated
    
    The console log is also outputing some unefined value i didnt expect.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -11,8 +11,8 @@ function getComputerChoice(){
 
 //Prompts the user for their choice. 
 function getHumanChoice(){
-    let myChoice = prompt('Choose between rock, paper or scissor and type it.Your opponent will be your computer :)', 'Type "rock", "paper" or "scissor"');
-    
+    let myChoice = prompt('Choose between rock, paper or scissor and type it. Your opponent will be your computer :)'
+        ,'Type "rock", "paper" or "scissor"').toLowerCase();
     if (myChoice != "rock" && myChoice != "paper" && myChoice != "scissor"){
         alert('Looks like there was a mistake somewhere.Please try again');
         return;
@@ -21,3 +21,53 @@ function getHumanChoice(){
     return myChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice , computerChoice){
+    const player = humanChoice;
+    const cpu = computerChoice;
+    if (player == cpu) {
+        return console.log('This is a tie')
+    }
+    //Comparison for computers win, humans loss
+    if ( 
+        ((player == "rock" )   && (cpu == "paper"))   || 
+        ((player == "paper")   && (cpu == "scissor")) ||
+        ((player == "scissor") && (cpu == "rock"))
+    ) {
+        console.log(`Skynet has won this round. ${cpu} beats ${player}`);
+        ++computerScore;
+        return
+        }
+
+    //Comparison for humans win, computers loss
+    if ( 
+        ((cpu == "rock" )   && (player == "paper"))   || 
+        ((cpu == "paper")   && (player == "scissor")) ||
+        ((cpu == "scissor") && (player == "rock"))
+    ) {
+        
+        console.log(`The organics have triumphed. ${player} beats ${cpu}`);
+        ++humanScore;
+        return
+        }
+    
+    /*const cpuWon = (function(player,cpu){
+        if ( 
+            ((player == "rock" ) && (cpu == "paper")) || 
+            ((player == "paper") && (cpu == "scissor")) ||
+            ((player == "scissor" ) && (cpu == "rock"))
+        ) {
+            console.log(`Skynet has won this round. ${cpu} beats ${player}`);
+        }
+    })*/
+
+    /* 
+    Retrieve both the human and comp choices
+    Compare them to each other
+    Establish win, lose and draw conditions
+    Inform the result and give a point
+    */
+
+}
